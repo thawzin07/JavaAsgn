1bbf852ea0f3820e36744047399fbb0319558b9a
thawzin07 <THAWZIN.22@ICHAT.SP.EDU.SG>
2023-07-30 00:19:04 +0800

    test commit

---
+++ JavaAsgn/src/main/java/dbaccess/DBConnection.java
@@ -0,0 +1,27 @@
+package dbaccess;
+
+import java.sql.*;
+
+public class DBConnection {
+	public static Connection getConnection() {
+		String dbUrl="jdbc:mysql://localhost/java_pract6";
+		String dbUser="root";
+		String dbPassword="root1234";
+		String dbClass="com.mysql.jdbc.Driver";
+		
+		Connection connection=null;
+		try {
+			Class.forName(dbClass);
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			connection= DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
