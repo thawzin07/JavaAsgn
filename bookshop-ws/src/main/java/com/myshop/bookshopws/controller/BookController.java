ee5b5f137bb1aa5640f3606fd99b9622c80ebe74
thawzin07 <THAWZIN.22@ICHAT.SP.EDU.SG>
2023-08-05 22:20:43 +0800

    update

--- bookshop-ws/src/main/java/com/myshop/bookshopws/controller/BookController.java
+++ bookshop-ws/src/main/java/com/myshop/bookshopws/controller/BookController.java
@@ -17,20 +17,71 @@ import com.myshop.bookshopws.model.Book;
 import com.myshop.bookshopws.model.BookDB;
 
 
+
 @RestController
 
 public class BookController {
-	@RequestMapping(method=RequestMethod.GET, path="/ListBookSales")
-	public ArrayList<Book> getUser() {
+	@RequestMapping(method=RequestMethod.GET, path="/ListTopSales")
+	public ArrayList<Book> getTopSales() {
 		ArrayList<Book> bookArray = new ArrayList<Book>();
+		
 		Book b = new Book();
 		try {
 			BookDB db = new BookDB();
-			bookArray = db.listBookSales();
+			bookArray = db.listTopSales();
 		} catch (Exception e) {
 			System.out.print("Error : "  + e);
 		}
 		return bookArray;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="/ListLeastSales")
+	public ArrayList<Book> getLeastSales() {
+		ArrayList<Book> bookArray = new ArrayList<Book>();
+		
+		Book b = new Book();
+		try {
+			BookDB db = new BookDB();
+			bookArray = db.listLeastSales();
+		} catch (Exception e) {
+			System.out.print("Error : "  + e);
+		}
+		return bookArray;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="/ListLowStocks")
+	public ArrayList<Book> getLowStocs() {
+		ArrayList<Book> bookArray = new ArrayList<Book>();
+		
+		Book b = new Book();
+		try {
+			BookDB db = new BookDB();
+			bookArray = db.listLowStock();
+		} catch (Exception e) {
+			System.out.print("Error : "  + e);
+		}
+		return bookArray;
+	}
+	
+	@RequestMapping(
+			path="/createBook",
+			consumes="application/json",
+			method = RequestMethod.POST)
+	public boolean createUser(@RequestBody Book book) {
+		boolean isCreated = false ;
+		try {
+			BookDB db = new BookDB();
+			String isbn = book.getISBN();
+			System.out.print("...inside BookController...Id: " + isbn);
+			String title = book.getTitle();
+			System.out.print("...inside BookController...Id: " + title);
+			isCreated = db.createBook(book);
+			
+			System.out.print("...done create user..." + isCreated);
+		}catch (Exception e) {
+			System.out.print("Error : "  + e.toString());
+		}
+		return isCreated ; //using the default ResponseBody with custom status from Spring
 	}
 
 }
--- bookshop-ws/src/main/java/com/myshop/bookshopws/model/BookDB.java
+++ bookshop-ws/src/main/java/com/myshop/bookshopws/model/BookDB.java
@@ -6,21 +6,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.myshop.bookshopws.model.Book;
+import  com.myshop.bookshopws.model.DBConnection;
+
 
 public class BookDB {
-	public ArrayList<Book> listBookSales () throws SQLException {
-		Book uBean = null ;
+	public ArrayList<Book> listTopSales() throws SQLException {
+		Book uBean = null;
 		Connection conn = null;
 		ArrayList<Book> books = new ArrayList<Book>();
-		
+
 		try {
 			conn = DBConnection.getConnection();
-			String sqlStr = "SELECT * FROM javaassignment.book ORDER BY sold_count DESC LIMIT 5";
+			String sqlStr = "SELECT * FROM javaassignment.book ORDER BY sold_count DESC LIMIT 3";
 			PreparedStatement pstmt = conn.prepareStatement(sqlStr);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-		     while (rs.next()) {
+
+			while (rs.next()) {
 				uBean = new Book();
 				uBean.setId(Integer.parseInt(rs.getString("id")));
 				uBean.setISBN(rs.getString("ISBN"));
@@ -30,21 +33,167 @@ public class BookDB {
 				uBean.setQuantity(Integer.parseInt(rs.getString("quantity")));
 				uBean.setPublisher(rs.getString("publisher"));
 				uBean.setPublication_date(rs.getString("publication_date"));
-				
+
 				uBean.setCat_id(Integer.parseInt(rs.getString("cat_id")));
-				uBean.setImage(rs.getString("image"));
+				//uBean.setImage(rs.getString("image"));
 				uBean.setSold_count(Integer.parseInt(rs.getString("sold_count")));
-				
+
 				books.add(uBean);
 				System.out.print("....done writing to bean!....");
 
 			}
 		} catch (Exception e) {
-			System.out.print("..........UserDetailsDB:" + e);
+			System.out.print("..........BookDetailsDB:" + e);
 		} finally {
 			conn.close();
 		}
 
 		return books;
+	}
+	
+	public ArrayList<Book> listLeastSales() throws SQLException {
+		Book uBean = null;
+		Connection conn = null;
+		ArrayList<Book> books2 = new ArrayList<Book>();
+
+		try {
+			conn = DBConnection.getConnection();
+			String sqlStr = "SELECT * FROM javaassignment.book ORDER BY sold_count ASC LIMIT 3";
+
+			PreparedStatement pstmt = conn.prepareStatement(sqlStr);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				uBean = new Book();
+				uBean.setId(Integer.parseInt(rs.getString("id")));
+				uBean.setISBN(rs.getString("ISBN"));
+				uBean.setTitle(rs.getString("title"));
+				uBean.setAuthor(rs.getString("author"));
+				uBean.setPrice(Double.parseDouble(rs.getString("price")));
+				uBean.setQuantity(Integer.parseInt(rs.getString("quantity")));
+				uBean.setPublisher(rs.getString("publisher"));
+				uBean.setPublication_date(rs.getString("publication_date"));
+
+				uBean.setCat_id(Integer.parseInt(rs.getString("cat_id")));
+				//uBean.setImage(rs.getString("image"));
+				uBean.setSold_count(Integer.parseInt(rs.getString("sold_count")));
+
+				books2.add(uBean);
+				System.out.print("....done writing to bean!....");
+
+			}
+		} catch (Exception e) {
+			System.out.print("..........BookDetailsDB:" + e);
+		} finally {
+			conn.close();
+		}
+
+		return books2;
+	}
+	
+	public ArrayList<Book> listLowStock() throws SQLException {
+		Book uBean = null;
+		Connection conn = null;
+		ArrayList<Book> books3 = new ArrayList<Book>();
+
+		try {
+			conn = DBConnection.getConnection();
+			String sqlStr = "SELECT * FROM javaassignment.book WHERE quantity < 5 ORDER BY quantity ASC" ;
+
+			PreparedStatement pstmt = conn.prepareStatement(sqlStr);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				uBean = new Book();
+				uBean.setId(Integer.parseInt(rs.getString("id")));
+				uBean.setISBN(rs.getString("ISBN"));
+				uBean.setTitle(rs.getString("title"));
+				uBean.setAuthor(rs.getString("author"));
+				uBean.setPrice(Double.parseDouble(rs.getString("price")));
+				uBean.setQuantity(Integer.parseInt(rs.getString("quantity")));
+				uBean.setPublisher(rs.getString("publisher"));
+				uBean.setPublication_date(rs.getString("publication_date"));
+
+				uBean.setCat_id(Integer.parseInt(rs.getString("cat_id")));
+				//uBean.setImage(rs.getString("image"));
+				uBean.setSold_count(Integer.parseInt(rs.getString("sold_count")));
+
+				books3.add(uBean);
+				System.out.print("....done writing to bean!....");
+
+			}
+		} catch (Exception e) {
+			System.out.print("..........BookDetailsDB:" + e);
+		} finally {
+			conn.close();
+		}
+
+		return books3;
+	}
+	
+	public boolean createBook(Book book) {
+
+		Connection conn = null;
+		boolean isSuccess = false;
+
+		try {
+			conn = DBConnection.getConnection();
+
+			String query = "SELECT COUNT(*) FROM javaassignment.book WHERE ISBN = ? OR title = ?";
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setString(1, book.getISBN());
+			statement.setString(2, book.getTitle());
+
+			// Execute the query
+			ResultSet resultSet = statement.executeQuery();
+			// Retrieve the count of matching records
+			int count = 0;
+			if (resultSet.next()) {
+				count = resultSet.getInt(1);
+			}
+
+			// Check the count and take appropriate action
+			if (count > 0) {
+				// A record with the same ISBN and title already exists
+				// Perform any necessary error handling or return false to indicate failure
+				return false;
+			} else {
+				String sql = "INSERT INTO javaassignment.book (isbn, title, author, price, quantity, publisher, publication_date, cat_id, image) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+
+				// Set parameter values
+				pstmt.setString(1, book.getISBN());
+				pstmt.setString(2, book.getTitle());
+				pstmt.setString(3, book.getAuthor());
+				pstmt.setDouble(4, book.getPrice());
+				pstmt.setInt(5, book.getQuantity());
+				pstmt.setString(6, book.getPublisher());
+				pstmt.setString(7, book.getPublication_date());
+				pstmt.setInt(8, book.getCat_id());
+				pstmt.setString(9, book.getImage());
+				// Execute the INSERT query
+				int rowsAffected = pstmt.executeUpdate();
+				// Close resources
+				pstmt.close();
+				if (rowsAffected > 0) {
+					// Book inserted successfully
+					isSuccess = true;
+				}
+			}
+		} catch (Exception e) {
+			System.out.print("..........BookDetailsDB:" + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return isSuccess;
 	}
 }
